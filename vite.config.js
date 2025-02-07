43032a5ea7a11269e7e84663071e4a9992c21128
Kishore S R <kishoresr01@gmail.com>
2025-02-07 11:59:08 -0500

    add: PWA Feature

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Top Hackathons",
+        short_name: "Hackathons",
+        description: "Explore and participate in hackathons easily.",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#58bf43",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        screenshots: [
+          {
+            src: "/screenshot.png",
+            sizes: "1080x1920",
+            type: "image/png",
+            form_factor: "narrow",
+          },
+        ],
+      },
+    }),
+  ],
+  server: {
+    open: true,
+  },
+  build: {
+    manifest: true,
+  },
+});
