15a44f84a5e97bf8c331e8cbdb11a45ca164bb07
Kishore S R <kishoresr01@gmail.com>
2025-02-14 23:43:17 -0500

    update: styles

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3vmlhhopr78"
+    "revision": "0.lfue5soo9g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
